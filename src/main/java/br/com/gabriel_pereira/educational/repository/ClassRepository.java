5f85a37cdc466cf53a6ef56764db131167a7db3a
Gabriel Carmo <gabrielcarmobr19@gmail.com>
2024-11-23 09:56:41 -0300

    Criação de Turmas

---
+++ src/main/java/br/com/gabriel_pereira/educational/repository/ClassRepository.java
@@ -0,0 +1,22 @@
+package br.com.gabriel_pereira.educacional.repository;
+
+import br.com.gabriel_pereira.educacional.model.ClassModel;
+import br.com.gabriel_pereira.educacional.model.CourseModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
+
+public interface ClassRepository extends JpaRepository<ClassModel, Integer> {
+        @Query("SELECT c FROM turmas c " +
+                "WHERE (:year IS NULL OR c.year = :year) " +
+                "AND (:semester IS NULL OR c.semester = :semester) " +
+                "AND (:courseId IS NULL OR c.courseModel.id = :courseId)")
+        Set<ClassModel> searchClasses(
+                @Param("year") Integer year,
+                @Param("semester") Integer semester,
+                @Param("courseId") Integer courseId
+        );
+}
+
---
+++ src/main/java/br/com/gabriel_pereira/educational/service/ClassService.java
@@ -0,0 +1,105 @@
+package br.com.gabriel_pereira.educacional.service;
+
+import br.com.gabriel_pereira.educacional.dto.ClassDto;
+import br.com.gabriel_pereira.educacional.dto.CourseDto;
+import br.com.gabriel_pereira.educacional.model.ClassModel;
+import br.com.gabriel_pereira.educacional.repository.ClassRepository;
+import br.com.gabriel_pereira.educacional.service.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class ClassService {
+
+    private final ClassRepository classRepository;
+
+    private final CourseService courseService;
+
+    public ClassService(ClassRepository classRepository, CourseService courseService) {
+        this.classRepository = classRepository;
+        this.courseService = courseService;
+    }
+
+    public void createClass(ClassDto classDto) {
+
+        CourseDto courseDto = getCourseDto(classDto);
+
+        classDto.setCourseDto(courseDto);
+
+        ClassModel classModel = CONVERT_DTO_TO_MODEL(classDto);
+
+        classRepository.save(classModel);
+    }
+
+    public void updateClass(ClassDto classDto, Integer id) {
+
+        CourseDto courseDto = getCourseDto(classDto);
+
+        classDto.setCourseDto(courseDto);
+
+        ClassModel classModel = CONVERT_DTO_TO_MODEL(classDto);
+        classModel.setId(id);
+        classRepository.save(classModel);
+    }
+
+    public ClassDto findById (Integer id) {
+        ClassModel classModel = classRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Id não foi encontrado"));
+
+        return CONVERT_MODEL_TO_DTO(classModel);
+    }
+
+    public Set<ClassDto> searchClass (Integer year, Integer semester, Integer courseId) {
+        Set<ClassModel> lstClassModel = classRepository.searchClasses(year, semester, courseId);
+        Set<ClassDto> lstClassDto = new HashSet<>();
+
+        for(ClassModel classModel : lstClassModel) {
+            lstClassDto.add(CONVERT_MODEL_TO_DTO(classModel));
+        }
+
+        return lstClassDto;
+    }
+
+    public void deleteClass(Integer id) {
+        ClassDto verifyClassDto = findById(id);
+        ClassModel classModel = CONVERT_DTO_TO_MODEL(verifyClassDto);
+        classModel.setId(id);
+        classRepository.delete(classModel);
+    }
+
+    private CourseDto getCourseDto(ClassDto classDto) {
+        CourseDto courseDto;
+        try {
+            courseDto = courseService.findById(classDto.getCourseId());
+        } catch (ResourceNotFoundException ex) {
+            throw new ResourceNotFoundException("curso_id não foi encontrado");
+        }
+        return courseDto;
+    }
+
+    public static ClassModel CONVERT_DTO_TO_MODEL(ClassDto classDto) {
+        ClassModel classModel = new ClassModel();
+        classModel.setId(classDto.getId() != null ? classDto.getId() : null);
+        classModel.setYear(classDto.getYear());
+        classModel.setSemester(classDto.getSemester());
+        classModel.setCourseModel(classDto.getCourseDto() != null ? CourseService.CONVERT_DTO_TO_MODEL(classDto.getCourseDto()) : null);
+
+        return classModel;
+    }
+
+    public static ClassDto CONVERT_MODEL_TO_DTO(ClassModel classModel) {
+        ClassDto classDto = new ClassDto();
+        classDto.setId(classModel.getId() != null ? classModel.getId() : null);
+        classDto.setYear(classModel.getYear());
+        classDto.setSemester(classModel.getSemester());
+        classDto.setCourseId(classModel.getCourseModel().getId() != null ? classModel.getCourseModel().getId() : null);
+        classDto.setCourseDto(classModel.getCourseModel() != null ? CourseService.CONVERT_MODEL_TO_DTO(classModel.getCourseModel()) : null);
+
+        return classDto;
+    }
+
+}
